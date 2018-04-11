f1bc98009000f9b501eda2d66fb3ad0fceb5fc31
vagrant <vagrant@homestead>
2018-04-11 15:52:59 +0000

    Finish statics pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,14 @@
+{{--<html>--}}
+{{--<head>--}}
+    {{--<title>Sample App</title>--}}
+{{--</head>--}}
+{{--<body>--}}
+   {{--<h1>关于</h1>--}}
+{{--</body>--}}
+{{--</html>--}}
+
+@extends('layouts.default')
+@section('title','关于')
+@section('content')
+    <h1>关于</h1>
+    @stop
